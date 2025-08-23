4cf256371e05a0851c1d962cfa75c67f0778a111
yatinpachori <67282395+yatinpachori@users.noreply.github.com>
2025-08-23 22:06:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ function random() {
   setTimeout(() => {
     const result = Math.random() < 0.5 ? "Heads" : "Tails";
     display.textContent = result;
+    console.log(result);
 
     if (result === "Tails") {
       coin.style.backgroundImage = `url('tails.svg')`;
@@ -25,4 +26,5 @@ function random() {
 
 button.addEventListener("click", random);
 coin.addEventListener("click", random);
+
 
